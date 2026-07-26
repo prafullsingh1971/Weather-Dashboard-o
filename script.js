1bc3b5728dcac9a42ec7549950e55d4e449ad7c6
prafullsingh1971 <prafullsingh1971@gmail.com>
2026-07-26 21:03:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //  STATE
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 const state = {
-    apiKey:"830889ef400efb1f29e0ab83d5464aa7",
+    apiKey:'830889ef400efb1f29e0ab83d5464aa7',
     unit: 'metric', // 'metric' | 'imperial'
     city: 'London',
     data: null, // current weather
